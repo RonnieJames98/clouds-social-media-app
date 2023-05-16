6d695a1c3f72803a1514182ea11ea2b399604ed9
Ronnie James <ronniejespinosa1998@gmail.com>
2023-05-16 13:49:59 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect}  from 'react';
+import {Routes, Route, useNavigate } from 'react-router-dom';
 
-function App() {
+
+import Login from './components/Login';
+import Home from './container/Home';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { fetchUser } from './utils/fetchUser';
+
+function App(){
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = fetchUser();
+
+    if (!user) navigate('/login');
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <GoogleOAuthProvider clientId="1078745229419-9sphtj3tkfr70ssemhoc54g9v0lesdpm.apps.googleusercontent.com">
+      <Routes>
+        <Route path="login" element={<Login />}/>
+        <Route path="/*" element={<Home />}/>
+
+      </Routes>
+    </GoogleOAuthProvider>
+  )
 }
 
 export default App;
